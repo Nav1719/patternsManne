25fc19a55a0f750bdb4140e4629dc03c4b3dc0f0
Naveen Manne <113809829+Nav1719@users.noreply.github.com>
2023-11-21 17:53:19 -0600

    first
    naveen

---
+++ src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,11 @@
+package com.bandiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+/*
+ * @Naveen Manne s563087
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
